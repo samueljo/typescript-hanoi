d7d96caed5521cb2e5b02ee7c640593d33d43a52
Sam Jo <samjo891@gmail.com>
2016-12-20 15:37:55 -0500

    Write game logic & compile to js

---
+++ js/game.js
@@ -0,0 +1,39 @@
+"use strict";
+var Game = (function () {
+    function Game() {
+    }
+    Game.prototype.contsructor = function () {
+        this.towers = [[3, 2, 1], [], []];
+    };
+    Game.prototype.isValidMove = function (startTowerIdx, endTowerIdx) {
+        var startTower = this.towers[startTowerIdx];
+        var endTower = this.towers[endTowerIdx];
+        if (startTower.length === 0) {
+            return false;
+        }
+        else if (endTower.length === 0) {
+            return true;
+        }
+        else {
+            var topStartDisc = startTower[startTower.length - 1];
+            var topEndDisc = endTower[endTower.length - 1];
+            return topStartDisc < topEndDisc;
+        }
+    };
+    Game.prototype.isWon = function () {
+        return (this.towers[2].length === 3) || (this.towers[1].length === 3);
+    };
+    Game.prototype.move = function (startTowerIdx, endTowerIdx) {
+        if (this.isValidMove(startTowerIdx, endTowerIdx)) {
+            this.towers[endTowerIdx].push(this.towers[startTowerIdx].pop());
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    return Game;
+}());
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = Game;
+//# sourceMappingURL=game.js.map
---
+++ ts/game.ts
@@ -0,0 +1,35 @@
+export default class Game {
+  towers: number[][];
+
+  contsructor() {
+    this.towers = [[3, 2, 1], [], []];
+  }
+
+  isValidMove(startTowerIdx: number, endTowerIdx: number): boolean {
+    const startTower: number[] = this.towers[startTowerIdx];
+    const endTower: number[] = this.towers[endTowerIdx];
+
+    if (startTower.length === 0) {
+      return false;
+    } else if (endTower.length === 0) {
+      return true;
+    } else {
+      const topStartDisc: number = startTower[startTower.length - 1];
+      const topEndDisc: number = endTower[endTower.length - 1];
+      return topStartDisc < topEndDisc;
+    }
+  }
+
+  isWon(): boolean {
+    return (this.towers[2].length === 3) || (this.towers[1].length === 3);
+  }
+
+  move(startTowerIdx: number, endTowerIdx: number): boolean {
+    if (this.isValidMove(startTowerIdx, endTowerIdx)) {
+      this.towers[endTowerIdx].push(this.towers[startTowerIdx].pop());
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
